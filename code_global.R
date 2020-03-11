ced241a6273cdc08358bc0b5899ffd60adb2e0ef
Wouter Meuleman <meuleman@gmail.com>
2020-03-11 12:21:10 -0700

    Code development

--- code_global.R
+++ code_global.R
@@ -47,8 +47,41 @@ options(scipen=2)
 
 ############################################################################################################################
 
-# Plot absolute number of confirmed cases for each of the top 9 countries
-fn <- "absolute_numbers_top9_min100"
+# Plot absolute number of confirmed cases for each of the top 20 countries
+fn <- "absolute_numbers_top20_min100"
+plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=8)
+layout(matrix(1:2, ncol=2), width=c(7,3))
+par(oma=c(2,2,1,0), bg="white", cex=2)
+par(mar=c(0,0,0,4))
+# Confirmed cases
+dat_to_plot <- confirmed[idxs,]
+dat_to_plot[dat_to_plot==0] <- NA
+#xlim=as.Date(c("2020-02-15", tail(colnames(confirmed), 1)))
+xlim=range(as.Date(colnames(confirmed)))
+plot(as.Date(colnames(confirmed)), rep(1, ncol(confirmed)), 
+     type="n", yaxt="n", xaxs="i", yaxs="i", xlab="", ylab="", log="y", xlim=xlim,
+     ylim=range(dat_to_plot[,as.Date(colnames(dat_to_plot)) %in% xlim], na.rm=T))
+for (i in 1:length(idxs)) {
+  lines(as.Date(colnames(confirmed)), confirmed[idxs[i],], col=cols[i], lwd=5)
+}
+axis(1, labels=F, at=as.Date(colnames(recovered)), tcl=-0.25)
+axis(4, las=2)
+mtext("# of confirmed cases", side=2, line=0.5, cex=2)
+legend("topleft", "(x,y)", "Confirmed cases", inset=c(-0.08,-0.02), bty="n", cex=1.25, text.font=4)
+par(mar=c(0,0,0,0))
+plot(0, type="n", axes=FALSE)
+legend("topleft", "(x,y)", string_date, inset=c(-0.12,-0.02), bty="n", cex=1.25, text.font=4)
+labs <- rownames(confirmed)[idxs]
+nums <- signif(confirmed[idxs,ncol(confirmed)], 2)
+ord <- order(nums, decreasing=TRUE)
+legend("topleft", "(x,y)", labs[ord], lwd=5, cex=0.7, col=cols[ord], inset=c(0.01, 0.1), bty="n")
+legend("topright", "(x,y)", nums[ord], cex=0.7, inset=c(0.01, 0.1), bty="n")
+dev.off()
+if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
+  system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf PNG_figures/", fn, "_latest.png", sep=""))
+}
+
+fn <- "absolute_numbers_top20_min100"
 plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=8)
 layout(matrix(1:2, ncol=2), width=c(7,3))
 par(oma=c(2,2,1,0), bg="white", cex=2)
@@ -82,7 +115,7 @@ if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
 }
 
 
-fn <- "percentage_population_confirmed_top9_min100"
+fn <- "percentage_population_confirmed_top20_min100"
 plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=8)
 layout(matrix(1:2, ncol=2), width=c(7,3))
 par(oma=c(2,2,1,0), bg="white", cex=2)
@@ -115,7 +148,7 @@ if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
   system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf PNG_figures/", fn, "_latest.png", sep=""))
 }
 
-fn <- "percentage_population_confirmed_top9_min100_log"
+fn <- "percentage_population_confirmed_top20_min100_log"
 plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=8)
 layout(matrix(1:2, ncol=2), width=c(7,3))
 par(oma=c(2,2,1,0), bg="white", cex=2)
@@ -148,7 +181,7 @@ if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
   system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf PNG_figures/", fn, "_latest.png", sep=""))
 }
 
-fn <- "percentage_population_deaths_recovered_top9_min100"
+fn <- "percentage_population_deaths_recovered_top20_min100"
 plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=4)
 par(mar=c(2,2,1,4), mfrow=c(1,2), cex=3, bg="white")
 # Deaths
@@ -182,7 +215,7 @@ if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
   system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf PNG_figures/", fn, "_latest.png", sep=""))
 }
 
-fn <- "percentage_cases_deaths_recovered_top9_min100"
+fn <- "percentage_cases_deaths_recovered_top20_min100"
 plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=4)
 par(mar=c(2,2,1,4), mfrow=c(1,2), cex=3, bg="white")
 # Plot percentage of cases resulting in deaths
--- code_selection.R
+++ code_selection.R
@@ -1,3 +1,6 @@
+############################################################################################################################
+### Code for plotting a number of custom derivative views
+############################################################################################################################
 library(RColorBrewer)
 source("code_preprocess.R")
 
@@ -31,9 +34,6 @@ confirmed[nam,] <- colSums(confirmed_all[idxs,-c(1:4)])
 deaths[nam,] <- colSums(deaths_all[idxs,-c(1:4)])
 recovered[nam,] <- colSums(recovered_all[idxs,-c(1:4)])
 
-#confirmed_all_pop[confirmed_all_pop$country=="Washington State", ncol(confirmed_all_pop)] <- 176
-#confirmed_all_pop[confirmed_all_pop$country=="King County, WA", ncol(confirmed_all_pop)] <- 116
-
 ############################################################################################################################
 
 ### Percentage of population with confirmed cases / deaths / recoveries
@@ -44,8 +44,8 @@ recovered_perc <- sweep(recovered, 1, population, FUN="/") * 100
 ### Percentage of confirmed cases that have resulted in deaths / recoveries
 confirmed_deaths_perc <- deaths / confirmed * 100
 confirmed_recovered_perc <- recovered / confirmed * 100
-confirmed_deaths_perc[confirmed < 50] <- NA # minimum of 50 cases required
-confirmed_recovered_perc[confirmed < 50] <- NA # minimum of 50 cases required
+confirmed_deaths_perc[confirmed < 100] <- NA # minimum of 100 cases required
+confirmed_recovered_perc[confirmed < 100] <- NA # minimum of 100 cases required
 
 ### Construct date string for showing in figures.
 string_date <- format(as.Date(tail(colnames(confirmed_perc), 1)), format="%B %d, %Y")
@@ -58,17 +58,14 @@ recovered_perc_mean <- colSums(recovered) / sum(population) * 100
 confirmed_deaths_perc_mean <- colSums(deaths) / colSums(confirmed) * 100
 confirmed_recovered_perc_mean <- colSums(recovered) / colSums(confirmed) * 100
 
-# Obtain "top-scoring" countries, in terms of percentage of population affected (min. 50 cases)
-#min50 <- which(confirmed[,ncol(confirmed)] > 50)
-#idxs <- head(order(-confirmed_perc[,ncol(confirmed_perc)]), 9)
-#idxs <- head(intersect(order(-confirmed_perc[,ncol(confirmed_perc)]), min50), 9)
+# Selection of countries
 idxs <- which(rownames(confirmed_perc) %in% c("Netherlands", "Italy", "Washington State", "King County, WA"))
 cols <- brewer.pal(9, "Set1")
 cols <- cols[1:length(idxs)]
 
 ############################################################################################################################
 
-fn <- "percentage_population_confirmed_top9_min50"
+fn <- "percentage_population_confirmed_custom"
 plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=8)
 par(mar=c(2,4,1,5), bg="white", cex=2)
 # Confirmed cases
@@ -85,26 +82,29 @@ legend("topleft", "(x,y)", "Confirmed cases", inset=c(-0.05,0.005), bty="n", cex
 legend("topleft", "(x,y)", rownames(confirmed_perc)[idxs], lwd=5, col=cols, inset=c(0.01, 0.1), bty="n")
 box()
 dev.off()
-#if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
-#  system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf ", fn, "_latest.png", sep=""))
-#}
+if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
+  system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf PNG_figures/", fn, "_latest.png", sep=""))
+}
 
 
 # Plot percentage weekly increase in confirmed cases, for each of (a new set of) top 9 countries
-fn <- "percentage_weekly_change_cases_confirmed_newtop9_min50"
+fn <- "percentage_weekly_change_cases_confirmed_top20"
 plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=8)
 par(mar=c(2,3,1,4), bg="white", cex=2)
-# Confirmed cases, percent change per day
-
-confirmed_all_min50 <- confirmed_all[, -c(1:4)]
-confirmed_all_min50[confirmed_all_min50 < 50] <- NA # a minimum of 10 cases per region/country
-n <- ncol(confirmed_all_min50);
-confirmed_all_perc_diff <- (t(apply(confirmed_all_min50, 1, diff, lag=7)) / confirmed_all_min50[,-c((n-6):n)]) * 100
+# Confirmed cases, percent change per week
+confirmed_all_min100 <- confirmed_all[, -c(1:4)]
+confirmed_all_min100[confirmed_all_min100 < 100] <- NA # a minimum of 10 cases per region/country
+n <- ncol(confirmed_all_min100);
+confirmed_all_perc_diff <- (t(apply(confirmed_all_min100, 1, diff, lag=7)) / confirmed_all_min100[,-c((n-6):n)]) * 100
+colnames(confirmed_all_perc_diff) <- colnames(confirmed_all_min100[,-c(1:7)])
 rownames(confirmed_all_perc_diff) <- gsub("^ - ", "", paste(confirmed_all$Province.State, confirmed_all$Country.Region, sep=" - "))
-idxs <- head(order(apply(confirmed_all_perc_diff, 1, median, na.rm=T), decreasing=TRUE), 9)
+#idxs <- head(order(apply(confirmed_all_perc_diff, 1, median, na.rm=T), decreasing=TRUE), 9) # median weekly increase
+idxs <- head(order(confirmed_all_perc_diff[,ncol(confirmed_all_perc_diff)], decreasing=TRUE), 9) # current weekly increase
+cols <- brewer.pal(9, "Set1")
+xlim <- as.Date(c("2020-03-01", tail(colnames(confirmed), 1)))
 plot(as.Date(colnames(confirmed_all_perc_diff)), rep(1, ncol(confirmed_all_perc_diff)), 
-     type="n", yaxt="n", xaxs="i", yaxs="i", ylim=range(confirmed_all_perc_diff[idxs,], na.rm=T), 
-     xlab="", ylab="", main=string_date)
+     type="n", yaxt="n", xaxs="i", yaxs="i", xlab="", ylab="", main=string_date,
+     xlim=xlim, ylim=range(confirmed_all_perc_diff[idxs,], na.rm=T))
 for (i in 1:length(idxs)) {
   lines(as.Date(colnames(confirmed_all_perc_diff)), confirmed_all_perc_diff[idxs[i],], col=cols[i], lwd=5)
 }
@@ -117,7 +117,41 @@ legend("topleft", "(x,y)", rownames(confirmed_all_perc_diff)[idxs], lwd=5,
 box()
 dev.off()
 if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
-  system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf ", fn, "_latest.png", sep=""))
+  system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf PNG_figures/", fn, "_latest.png", sep=""))
+}
+
+
+# Plot percentage daily increase in confirmed cases, for each of (a new set of) top 9 countries
+fn <- "percentage_daily_change_cases_confirmed_top20"
+plotfile(paste(figdir, fn, sep="/"), type="pdf", width=14, height=8)
+par(mar=c(2,3,1,4), bg="white", cex=2)
+# Confirmed cases, percent change per day
+confirmed_all_min100 <- confirmed_all[, -c(1:4)]
+confirmed_all_min100[confirmed_all_min100 < 100] <- NA # a minimum of 10 cases per region/country
+n <- ncol(confirmed_all_min100);
+confirmed_all_perc_diff <- (t(apply(confirmed_all_min100, 1, diff, lag=1)) / confirmed_all_min100[,-n]) * 100
+colnames(confirmed_all_perc_diff) <- colnames(confirmed_all_min100[,-c(1:1)])
+rownames(confirmed_all_perc_diff) <- gsub("^ - ", "", paste(confirmed_all$Province.State, confirmed_all$Country.Region, sep=" - "))
+#idxs <- head(order(apply(confirmed_all_perc_diff, 1, median, na.rm=T), decreasing=TRUE), 9) # median daily increase
+idxs <- head(order(confirmed_all_perc_diff[,ncol(confirmed_all_perc_diff)], decreasing=TRUE), 9) # current daily increase
+cols <- brewer.pal(9, "Set1")
+xlim <- as.Date(c("2020-03-01", tail(colnames(confirmed), 1)))
+plot(as.Date(colnames(confirmed_all_perc_diff)), rep(1, ncol(confirmed_all_perc_diff)), 
+     type="n", yaxt="n", xaxs="i", yaxs="i", xlab="", ylab="", main=string_date,
+     xlim=xlim, ylim=range(confirmed_all_perc_diff[idxs,], na.rm=T))
+for (i in 1:length(idxs)) {
+  lines(as.Date(colnames(confirmed_all_perc_diff)), confirmed_all_perc_diff[idxs[i],], col=cols[i], lwd=5)
+}
+axis(1, labels=F, at=as.Date(colnames(confirmed_all_perc_diff)), tcl=-0.25)
+axis(4, las=2)
+mtext("daily % increase in confirmed cases", side=2, line=0.5, cex=2)
+legend("topleft", "(x,y)", "Confirmed cases", inset=c(-0.05,0.005), bty="n", cex=1.25, text.font=4)
+legend("topleft", "(x,y)", rownames(confirmed_all_perc_diff)[idxs], lwd=5, 
+       col=cols, inset=c(0.01, 0.1), bty="n")
+box()
+dev.off()
+if (file.exists(paste(figdir, "/", fn, "_", id(), ".pdf", sep=""))) {
+  system(paste("convert -density 144 ", figdir, "/", fn, "_", id(), ".pdf PNG_figures/", fn, "_latest.png", sep=""))
 }
 
 # Other ideas:
